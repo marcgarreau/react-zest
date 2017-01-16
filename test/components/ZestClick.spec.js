7690b8480293ec89efdaef1a5b2915336c70041e
Marc Garreau <marcdgarreau@gmail.com>
2017-01-16 07:43:01 -0700

    Tests ZestClick and ZestHover

--- test/components/ZestClick.spec.js
+++ test/components/ZestClick.spec.js
@@ -1,3 +1,4 @@
+import 'jsdom-global/register';
 import React from 'react';
 import expect from 'expect';
 import { mount } from 'enzyme';
@@ -5,22 +6,10 @@ import ZestClick from '../../lib/components/triggerTypes/ZestClick';
 
 
 describe('Component: ZestClick', () => {
-  xit('toggles animation on click', () => {
+  it('toggles animation on click', () => {
     const spy = expect.createSpy();
     const wrapper = mount(<ZestClick toggleAnimation={spy} />);
     wrapper.find(ZestClick).simulate('click');
     expect(spy).toHaveBeenCalled();
   });
-  
-  // it('is initialized with the appropriate class names', () => {
-  //   const wrapper = shallow(<ZestClick />);
-  //   expect(wrapper.find('.react-zest').length).toEqual(1);
-  //   expect(wrapper.find('.zest-click').length).toEqual(0);
-  // });
-  //
-  // it('is applies the appropriate class names on click', () => {
-  //   const wrapper = shallow(<ZestClick />);
-  //   wrapper.find('.react-zest').simulate('click');
-  //   expect(wrapper.state().classNames).toEqual('react-zest zest-click');
-  // });
 });
--- test/components/ZestHover.spec.js
+++ test/components/ZestHover.spec.js
@@ -1,3 +1,4 @@
+import 'jsdom-global/register';
 import React from 'react';
 import expect from 'expect';
 import { mount } from 'enzyme';
@@ -5,22 +6,10 @@ import ZestHover from '../../lib/components/triggerTypes/ZestHover';
 
 
 describe('Component: ZestHover', () => {
-  xit('toggles the animation trigger on hover', () => {
+  it('toggles the animation trigger on hover', () => {
     const spy = expect.createSpy();
     const wrapper = mount(<ZestHover toggleAnimation={spy} />);
     wrapper.find(ZestHover).simulate('mouseEnter');
     expect(spy).toHaveBeenCalled();
   });
-  
-  // it('is initialized with the appropriate class names', () => {
-  //   const wrapper = shallow(<ZestHover />);
-  //   expect(wrapper.find('.react-zest').length).toEqual(1);
-  //   expect(wrapper.find('.zest-hover').length).toEqual(0);
-  // });
-  //
-  // it('is applies the appropriate class names on hover', () => {
-  //   const wrapper = shallow(<ZestHover />);
-  //   wrapper.find('.react-zest').simulate('mouseEnter');
-  //   expect(wrapper.state().classNames).toEqual('react-zest zest-hover');
-  // });
 });
