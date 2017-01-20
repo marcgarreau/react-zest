46aa5e87728df3dd5445c0eaff8b0e710e756c05
Marc Garreau <marcdgarreau@gmail.com>
2017-01-20 14:14:34 -0700

    Adds tests

--- test/components/Zest.spec.js
+++ test/components/Zest.spec.js
@@ -20,6 +20,12 @@ describe('Component: Zest', () => {
     expect(wrapper.find(ZestHover).length).toEqual(0);
   });
 
+  it('conditionally renders the ZestHover component', () => {
+    const wrapper = shallow(<Zest animationName="wiggle" animationTrigger="hover" />);
+    expect(wrapper.find(ZestHover).length).toEqual(1);
+    expect(wrapper.find(ZestClick).length).toEqual(0);
+  });
+
   it('allows users to specify animation duration', () => {
     const wrapper = shallow(<Zest animationName="wiggle" animationTrigger="click" animationDuration={300} />);
     expect(wrapper.find(ZestClick).props().animationDuration).toEqual(300);
